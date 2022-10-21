dbe37f5e27f3a47606df56ce6a3ba450a39049ea
Vanessa Power <vanessa.l.power@gmail.com>
2022-10-21 13:51:51 -0700

    writing join statements

--- 1_queries/all_student_names_single_cohort.sql
+++ 1_queries/all_student_names_single_cohort.sql
@@ -2,3 +2,9 @@ SELECT id, name
 FROM students
 WHERE cohort_id = 7
 ORDER BY name;
+
+-- SELECT students.name as student_name, cohorts.name as cohorts_name, cohorts.start_date as cohort_start_date, students.start_date as student_start_date 
+-- FROM students 
+-- JOIN cohorts ON students.cohort_id = cohorts.id 
+-- WHERE students.start_date != cohorts.start_date 
+-- ORDER BY cohorts.start_date
